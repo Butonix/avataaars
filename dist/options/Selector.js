5888e1af60282a9840bea70185909d5c980cdd9e
Dan Beneventano <danbeneventano@protonmail.com>
2020-07-09 10:24:37 -0400

    componentWillMount -> UNSAFE_componentWillMount

--- dist/options/Selector.js
+++ dist/options/Selector.js
@@ -39,7 +39,7 @@ var Selector = /** @class */ (function (_super) {
         enumerable: true,
         configurable: true
     });
-    Selector.prototype.componentWillMount = function () {
+    Selector.prototype.UNSAFE_componentWillMount = function () {
         var _a = this.props, option = _a.option, defaultOption = _a.defaultOption;
         var optionContext = this.optionContext;
         var defaultValue = (typeof defaultOption === 'string' ?
@@ -75,7 +75,7 @@ var Selector = /** @class */ (function (_super) {
             return;
         }
         var _a = this.props, option = _a.option, children = _a.children;
-        var values = React.Children.map(children, 
+        var values = React.Children.map(children,
         // TODO: also validate and throw error if we don't see optionValue
         function (child) { return getComponentOptionValue(child.type); });
         if (new Set(values).size !== values.length) {
